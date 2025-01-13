585e97111df7918f26d7fa1a5cadcfe920d03775
Alex Helms <alexander.helms@gmail.com>
2025-01-12 21:11:06 -0700

    Update fix_sqlite.sh to work for arm64

--- src/fix_sqlite.sh
+++ src/fix_sqlite.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/bash
 
-echo "Target arch: $TARGETARCH"
-if [ "$TARGETARCH" = "amd64" ]; then
+echo "Target arch: $1"
+if [ "$1" = "amd64" ]; then
     echo "Replacing x64 /lib/x86_64-linux-gnu/libsqlite3.so.0.8.6"
     cp /app/runtimes/linux-x64/native/libe_sqlite3.so /lib/x86_64-linux-gnu/libsqlite3.so.0.8.6
-elif [ "$TARGETARCH" = "arm64" ]; then
+elif [ "$1" = "arm64" ]; then
     echo "Replacing arm64 /lib/x86_64-linux-gnu/libsqlite3.so.0.8.6"
-    cp /app/runtimes/linux-arm64/native/libe_sqlite3.so /lib/x86_64-linux-gnu/libsqlite3.so.0.8.6
+    cp /app/runtimes/linux-arm64/native/libe_sqlite3.so /lib/aarch64-linux-gnu/libsqlite3.so.0.8.6
 else
-    echo "unhandled arch $TARGETARCH"
+    echo "unhandled arch $1"
 fi
