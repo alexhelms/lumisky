d0005b4decf5302ada84d23bc44d763ba25b8aa0
Alex Helms <alexander.helms@gmail.com>
2025-01-14 17:26:34 -0700

    Symlink sqlite dynlib instead of copying to /lib

--- src/fix_sqlite.sh
+++ src/fix_sqlite.sh
@@ -5,10 +5,12 @@
 echo "Target arch: $1"
 if [ "$1" = "amd64" ]; then
     echo "Replacing x64 /lib/x86_64-linux-gnu/libsqlite3.so.0.8.6"
-    cp /app/libe_sqlite3.so /lib/x86_64-linux-gnu/libsqlite3.so.0.8.6
+    rm /app/libe_sqlite3.so
+    ln -s /lib/x86_64-linux-gnu/libsqlite3.so.0.8.6 /app/libe_sqlite3.so
 elif [ "$1" = "arm64" ]; then
     echo "Replacing arm64 /lib/x86_64-linux-gnu/libsqlite3.so.0.8.6"
-    cp /app/libe_sqlite3.so /lib/aarch64-linux-gnu/libsqlite3.so.0.8.6
+    rm /app/libe_sqlite3.so
+    ln -s /lib/aarch64-linux-gnu/libsqlite3.so.0.8.6 /app/libe_sqlite3.so
 else
     echo "unhandled arch $1"
 fi
