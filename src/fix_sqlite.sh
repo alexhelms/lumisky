2250f2e0ed244e901b924969d5192bc525cdcb61
Alex Helms <alexander.helms@gmail.com>
2025-01-22 22:45:36 -0700

    Fix echo in fix_sqlite script

--- src/fix_sqlite.sh
+++ src/fix_sqlite.sh
@@ -8,7 +8,7 @@ if [ "$1" = "amd64" ]; then
     rm /app/libe_sqlite3.so
     ln -s /lib/x86_64-linux-gnu/libsqlite3.so.0.8.6 /app/libe_sqlite3.so
 elif [ "$1" = "arm64" ]; then
-    echo "Replacing arm64 /lib/x86_64-linux-gnu/libsqlite3.so.0.8.6"
+    echo "Replacing arm64 /lib/aarch64-linux-gnu/libsqlite3.so.0.8.6"
     rm /app/libe_sqlite3.so
     ln -s /lib/aarch64-linux-gnu/libsqlite3.so.0.8.6 /app/libe_sqlite3.so
 else
