189519e39f663cf9b27d6779c5fd5669b4578fad
Alex Helms <alexander.helms@gmail.com>
2025-01-23 19:41:42 -0700

    Add a helper bus handler for sending notifications from the message bus.

---
+++ src/LumiSky.Core/Handlers/NotificationHandler.cs
@@ -0,0 +1,15 @@
+﻿using LumiSky.Core.Services;
+using SlimMessageBus;
+
+namespace LumiSky.Core.Handlers;
+
+// Helper handler to send notifications from the message bus.
+
+public class NotificationHandler(NotificationService notificationService) :
+    IConsumer<NotificationMessage>
+{
+    public async Task OnHandle(NotificationMessage message)
+    {
+        await notificationService.SendNotification(message);
+    }
+}
